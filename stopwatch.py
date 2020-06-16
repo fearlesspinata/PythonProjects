471c7374fd9884380245d79dbf816ee801bd5c7b
Vesna Lay <vesnalay@gmail.com>
2020-06-16 01:17:54 -0700

    Adding some additional text

--- stopwatch.py
+++ stopwatch.py
@@ -2,10 +2,11 @@ import time
 
 input("Press enter to start the timer.")
 startTime = time.localtime()
+print(f"Timer started at {time.strftime('%X', startTime)}")
 
 input("Press enter to stop the timer.")
 endTime = time.localtime()
+print(f"Timer stopped at {time.strftime('%X', endTime)}")
 
 diff = time.mktime(endTime) - time.mktime(startTime)
-
-print(diff)
+print(f"The difference was {diff} seconds")
